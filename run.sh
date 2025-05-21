e8dacc58b844c7a629cd2e1754c1e760dfa354a6
Neutrino <ntrnntrn@git.dc>
2025-05-22 02:26:19 +0600

    Add non root user to container to correctly map user guid to host system Move nvim config to /local_store/nvim_config Bind ~/.ssh to container

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 docker run -it \
 	--mount type=bind,src=$HOME/projects,dst=/workspaces \
 	--mount type=volume,src=neovim-cpp-local-store,dst=/local_store  \
-	--mount "type=bind,source=$HOME/.config/nvim,target=/root/.config/nvim" \
+	--mount "type=bind,source=$HOME/.config/nvim,target=/local_store/nvim_config" \
+	--mount "type=bind,source=$HOME/.ssh,target=/local_store/.ssh" \
 	--memory=6gb \
 	--hostname=shengli_fh \
 	docker-neovim-cpp
