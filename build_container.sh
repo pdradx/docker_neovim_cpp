a2ac1669665bde8361de6079f7d16b2b3df0e085
Neutrino <ntrnntrn@git.dc>
2025-05-22 12:36:10 +0600

    Change workflow of working with container - execute new bash sessions inside the same container.

--- build_container.sh
+++ build_container.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-docker build -t docker-neovim-cpp ./.devcontainer
+IMAGE_NAME=docker-neovim-cpp
+CONTAINER_NAME=neovim-cpp
 
+docker build -t $IMAGE_NAME ./.devcontainer
+
+docker rm --force $CONTAINER_NAME
+
+docker run -d -t \
+    --name $CONTAINER_NAME \
+	--mount type=bind,src=$HOME/projects,dst=/workspaces \
+	--mount type=volume,src=neovim-cpp-local-store,dst=/local_store  \
+	--mount "type=bind,source=$HOME/.config/nvim,target=/local_store/nvim_config" \
+	--mount "type=bind,source=$HOME/.ssh,target=/local_store/.ssh" \
+	--memory=6gb \
+	--hostname=shengli_fh \
+    $IMAGE_NAME 
 
--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -it \
-	--mount type=bind,src=$HOME/projects,dst=/workspaces \
-	--mount type=volume,src=neovim-cpp-local-store,dst=/local_store  \
-	--mount "type=bind,source=$HOME/.config/nvim,target=/local_store/nvim_config" \
-	--mount "type=bind,source=$HOME/.ssh,target=/local_store/.ssh" \
-	--memory=6gb \
-	--hostname=shengli_fh \
-	docker-neovim-cpp
-# docker run -it docker-neovim-cpp --mount type=volume,src=<volume-name>,dst=<mount-path>
+CONTAINER_NAME=neovim-cpp
 
+docker start $CONTAINER_NAME
+docker exec -it $CONTAINER_NAME bash
 
