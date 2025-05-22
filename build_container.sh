f9e90dca03aa3c41f187364242d35dc9e2dc92c2
Neutrino <ntrnntrn@git.dc>
2025-05-22 07:09:43 +0000

    Set the hostname in the build command. Use tail -f as a background process to keep the container running.

--- build_container.sh
+++ build_container.sh
@@ -7,13 +7,16 @@ docker build -t $IMAGE_NAME ./.devcontainer
 
 docker rm --force $CONTAINER_NAME
 
-docker run -d -t \
+docker run -d \
     --name $CONTAINER_NAME \
 	--mount type=bind,src=$HOME/projects,dst=/workspaces \
 	--mount type=volume,src=neovim-cpp-local-store,dst=/local_store  \
 	--mount "type=bind,source=$HOME/.config/nvim,target=/local_store/nvim_config" \
 	--mount "type=bind,source=$HOME/.ssh,target=/local_store/.ssh" \
 	--memory=6gb \
-	--hostname=shengli_fh \
-    $IMAGE_NAME 
+	--hostname=neovim_cpp \
+    $IMAGE_NAME \
+    tail -f /dev/null
+
+
 
