1a38df8ffa942cef2267a7551dc71ae2dfe408a6
Neutrino <ntrnntrn@git.dc>
2025-07-30 13:43:30 +0600

    Connect to container in tmux session

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ set -ex  # Exit on error (-e) and print each command (-x) for debugging
 CONTAINER_NAME=neovim-cpp
 
 docker start $CONTAINER_NAME
-docker exec -it $CONTAINER_NAME bash
+# docker exec -it $CONTAINER_NAME bash
+docker exec -it $CONTAINER_NAME bash -i -c tmux
+# docker exec -it $CONTAINER_NAME bash -i -c "tmux new-session -A -s def"
 
