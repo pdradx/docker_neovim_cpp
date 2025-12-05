44c64b8422307304fd8cf8ab1fd19244579d281a
Neutrino <ntrnntrn@git.dc>
2025-12-06 00:29:02 +0600

    Add gcc-toolset-14-libasan-devel

--- .devcontainer/install_gcc_as_toolset.sh
+++ .devcontainer/install_gcc_as_toolset.sh
@@ -4,7 +4,7 @@ set -ex  # Exit on error (-e) and print each command (-x) for debugging
 dnf -y update
 dnf -y groupinstall "Development Tools"
 dnf -y install wget bzip2 tar xz gcc gcc-c++ make
-dnf -y install gcc-toolset-14
+dnf -y install gcc-toolset-14 gcc-toolset-14-libasan-devel
 
 echo "source /opt/rh/gcc-toolset-14/enable" > /etc/profile.d/gcc-toolset-14.sh
 chmod +x /etc/profile.d/gcc-toolset-14.sh
